27ebe0a987b07f2338f99393734067c23a3ebf07
adriencrove <ndeadrien@gmail.com>
2024-08-05 09:15:27 +0000

    [ADD] Ajout du champ selection / date de naissance/ Nom de la compagnie et des employés

--- app/(Auth)/signup/page.tsx
+++ app/(Auth)/signup/page.tsx
@@ -4,7 +4,10 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { CalendarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { useForm } from "react-hook-form"
@@ -12,8 +15,36 @@ import * as z from 'zod'
 
 const formSchema = z.object({
   email: z.string().email().min(1, { message: 'Email is required' }),
-  password: z.string().min(1, { message: 'Password is required' })
+  password: z.string().min(1, { message: 'Password is required' }),
+  account_type:z.enum(["Personal","Company"]),
+  company_name: z.string().optional(),
+  Employee_number:z.coerce.number().optional(),
+  birthday_date:z.date().refine((date)=>{
+    const today = new Date();
+    const Minor = new Date(
+      today.getFullYear()-18,
+      today.getMonth(),
+      today.getDate()
+    );
+    return date <= Minor;
+  }, "Vous devez avoir 18 ans"),
+}).superRefine((data, ctx) =>{
+    if(data.account_type==="Company" && !data.company_name){
+      ctx.addIssue({
+        code:z.ZodIssueCode.custom,
+        path: ["company_name"],
+        message: "Le nom de la compagnie est obligatoire",
+      })
+    }
+    if(data.account_type==="Company" && (!data.Employee_number|| data.Employee_number < 1)){
+      ctx.addIssue({
+        code:z.ZodIssueCode.custom,
+        path: ["Employee_number"],
+        message: "Le nombre d'employée de la compagnie est obligatoire",
+      })
+    }
 })
+
 
 export default function SignupPage() {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -27,6 +58,8 @@ export default function SignupPage() {
   const handleSubmit = () => {
     console.log("Validation de la connexion")
   }
+
+  const accountType= form.watch("account_type")
 
   return (
     <>
@@ -52,6 +85,70 @@ export default function SignupPage() {
                     <FormMessage/>
                   </FormItem>
                 )}/>
+                <FormField control={form.control} name="account_type" render={({field})=>(
+                  <FormItem>
+                    <FormLabel>Type de compte</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                     <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="selectionné un type de compte"/>
+                      </SelectTrigger>
+                     </FormControl>
+                     <SelectContent>
+                        <SelectItem value="Personal">Personel</SelectItem>
+                        <SelectItem value="Company">Entreprise</SelectItem>
+                      </SelectContent> 
+                    </Select>
+                  </FormItem>
+                )}/>
+                {accountType==="Company" &&
+                <>
+                  <FormField control={form.control} name="company_name"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Nom de la compagnie</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="text" placeholder="" />
+                      </FormControl>
+                      <CardDescription />
+                      <FormMessage/>
+                    </FormItem>
+                  )}/>
+                  <FormField control={form.control} name="Employee_number"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Nombre d"employée</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="number" min={0} placeholder="" />
+                      </FormControl>
+                      <CardDescription />
+                      <FormMessage/>
+                    </FormItem>
+                  )}/>
+                </>
+                }
+                <FormField control={form.control} name="birthday_date"
+                  render={({field}) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel>Date de naissance</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl> 
+                            <Button variant={"outline"} className="normal-case flex justify-between ">
+                              <span>Choisir une date</span>                            
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>                       
+
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                          
+                        </PopoverContent>
+                      </Popover>
+                      <CardDescription />
+                      <FormMessage/>
+                    </FormItem>
+                  )}/>
                 <FormField control={form.control} name="password"
                 render={({field}) => (
                   <FormItem>
